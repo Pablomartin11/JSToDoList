c221bd9253e2b7ac2b29aac2e24b57201d89cd31
pablomdb11 <pablomdb11@gmail.com>
2023-08-30 14:03:23 +0200

    localStorage implemented

---
+++ js/components/add-todo.js
@@ -0,0 +1,22 @@
+import Alert from "./alert.js";
+
+export default class AddTodo {
+
+    constructor(){
+    this.title = document.getElementById('title');
+    this.description = document.getElementById('description');
+    this.btn = document.getElementById("add");
+    this.alert = new Alert('alert');
+    }
+
+    onClick(callback){
+        this.btn.onclick = () => {
+            if (title.value === '' || description.value === ''){
+                this.alert.show('Title and description are requiered!');
+            } else {
+                this.alert.hide;
+                callback(this.title.value,this.description.value);
+            }
+        }
+    }
+}
